01cd172e362de162d1101418ac46f39d0a562b9a
Jeff Day <jeffrey.day00@gmail.com>
2019-09-11 14:14:09 -0700

    First laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>here we go!!!</h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more
+    </p>
+  </body>
+</html>
