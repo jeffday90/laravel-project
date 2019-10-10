755808d6b08105b61ca5fe1461e92c7bf0bcf6de
Jeff Day <jeffrey.day00@gmail.com>
2019-10-10 15:57:24 -0700

    Added blade layout file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1>here we go!!!</h1>
-    <p>
-        <ul>
-          <li><a href="/contact">Contact us</a> to learn more
-          <li><a href="/about">About us</a>
-        </ul>
-    </p>
-  </body>
-</html>
+@extends('layout')
+
+@section('content')
+  <h1>My first site</h1>
+@endsection
