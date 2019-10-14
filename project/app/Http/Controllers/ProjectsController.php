76ff28156dd97e429a7e95bcad8b15b404be40f8
Jeff Day <jeffrey.day00@gmail.com>
2019-10-14 13:22:39 -0700

    Querying database and passing data to view.

---
+++ project/app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// use this to shorten the naming convention so you can just use Project
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index() 
+    {
+        // a slash ('\') at the beginning of the route signifies to go to home directory 
+        // --> this is a PHP convention
+        // $projects = \App\Project::all();
+        
+        $projects = Project::all();
+
+        // interpets and returns JSON automatically
+        //return $projects;
+        
+        return view('projects.index', compact('projects'));
+    }
+}
